c92ff5614cd60f2d1303139e1a313fb6c2a60294
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2024-01-31 13:14:26 +0300

    added frame to change image size

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 import Homepage from "./components/home-page/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppBarComponent from "./components/app-bar/Appbar";
--- src/components/templates/canvas/Canvas.js
+++ src/components/templates/canvas/Canvas.js
@@ -4,13 +4,110 @@ import AlertTitle from "@mui/material/AlertTitle";
 import "./Canvas.css";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import { Typography } from "@mui/material";
+
 import Alert from "@mui/material/Alert";
-import IconButton from "@mui/material/IconButton";
-import Collapse from "@mui/material/Collapse";
-import CloseIcon from "@mui/icons-material/Close";
+
+import { render } from "react-dom";
+import { Stage, Layer, Image, Transformer } from "react-konva";
+import useImage from "use-image";
+import Konva from "konva";
+
+const Rectangle = ({
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+  clickOnUnsplash,
+}) => {
+  const [image] = useImage(clickOnUnsplash);
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useLayoutEffect(() => {
+    shapeRef.current.cache();
+  }, [image]);
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.setNode(shapeRef.current);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <React.Fragment>
+      <Image
+        image={image}
+        onClick={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        width={420}
+        height={560}
+        onTransformEnd={(e) => {
+          const node = shapeRef.current;
+
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            scaleX: node.scaleX(),
+            scaleY: node.scaleY(),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+const initialRectangles = [
+  {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100,
+    scaleX: 1,
+    scaleY: 1,
+    id: "rect1",
+  },
+];
 
 const Canvas = (props) => {
+  const [rectangles, setRectangles] = React.useState(initialRectangles);
+  const [selectedId, selectShape] = React.useState(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  });
+
+  const handleClick = (event) => {
+    if (event.toElement.toString() != "[object HTMLCanvasElement]")
+      selectShape(null);
+  };
+
   return (
     <>
       <Box
@@ -45,17 +142,41 @@ const Canvas = (props) => {
             </Alert>
           )}
           {props.unsplashImagesOnline && props.clickOnUnsplash && (
-            <div className="image">
-              <img
-                src={props.clickOnUnsplash}
-                alt="image"
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "cover",
-                  marginTop: "0px",
+            <div className="image" id="imageCanvasForRect">
+              <Stage
+                width={420}
+                height={560}
+                onMouseDown={(e) => {
+                  // deselect when clicked on empty area
+                  const clickedOnEmpty = e.target === e.target.getStage();
+                  let elem = document.getElementById("imageCanvasForRect");
+
+                  if (clickedOnEmpty) {
+                    selectShape(null);
+                  }
                 }}
-              />
+              >
+                <Layer>
+                  {rectangles.map((rect, i) => {
+                    return (
+                      <Rectangle
+                        key={i}
+                        shapeProps={rect}
+                        isSelected={rect.id === selectedId}
+                        onSelect={() => {
+                          selectShape(rect.id);
+                        }}
+                        clickOnUnsplash={props.clickOnUnsplash}
+                        onChange={(newAttrs) => {
+                          const rects = rectangles.slice();
+                          rects[i] = newAttrs;
+                          setRectangles(rects);
+                        }}
+                      />
+                    );
+                  })}
+                </Layer>
+              </Stage>
             </div>
           )}
         </Paper>
