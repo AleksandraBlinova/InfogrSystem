147d734266d4a56a7a5940d3ae06551effc67a1c
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2024-02-06 19:40:17 +0300

    addedStyles

--- src/components/templates/canvas/text-area/TextArea.js
+++ src/components/templates/canvas/text-area/TextArea.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 import { Stage, Layer, Text, Image } from "react-konva";
 
 const TextArea = ({ addText, textInputRef }) => {
@@ -31,6 +32,77 @@ const TextArea = ({ addText, textInputRef }) => {
           Добавить
         </Button>
       </Stack>
+      <div className="text-area-container">
+        <Typography
+          sx={{ fontSize: "16px", fontWeight: "500", marginBottom: "10px" }}
+        >
+          Стили текста:{" "}
+        </Typography>
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/обычный.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/жирный.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/наклонный.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/подчеркнутый.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/жирИнакл.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/жирИнаклИподч.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/жирИподч.png"
+        />
+        <img
+          style={{
+            width: "100px",
+            marginRight: "15px",
+            border: "1px solid grey",
+          }}
+          src="./textStyles/наклИподч.png"
+        />
+      </div>
     </div>
   );
 };
